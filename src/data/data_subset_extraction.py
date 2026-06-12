89fc381e1df4d3e1c205c118ac8c527b7ec47f46
Michela <m.deodati@studenti.unipi.it>
2026-06-12 17:01:50 +0200

    creazione del validation set e controllo che non abbinao nessun elemento in comiune

--- src/data/data_subset_extraction.py
+++ src/data/data_subset_extraction.py
@@ -36,15 +36,32 @@ def subset_selection (filename, row)->list:
                 break
     print(f"0: {c0}, 1:{c1}, totale estratti{len(subset)}")
     return subset
-                
+
+              
     
-def main (filename: str, row: int, output:str):
-    df  = pd.DataFrame(subset_selection(filename,row), columns=["text", "label"])
-    df = df.sample(frac=1).reset_index(drop=True)
-    # print(df)
-    df.to_csv(f"{output}", index=False)
-    print(f"File {output} salvato")
-    return
+def main (filename: str, row: int, output:str, data:str):
+    match data:
+        case "train":
+            df  = pd.DataFrame(subset_selection(filename,row), columns=["text", "label"])   
+            df = df.sample(frac=1).reset_index(drop=True)
+            df.to_csv(f"{output}", index=False)
+            print(f"File {output} salvato")
+            return
+        case "test:": 
+            df  = pd.DataFrame(subset_selection(filename,row), columns=["text", "label"])   
+            df = df.sample(frac=1).reset_index(drop=True)
+            df.to_csv(f"{output}", index=False)
+            print(f"File {output} salvato")
+            return
+        case "validation":
+            df=pd.read_csv(filename)
+            df = df.tail(row).iloc[::-1]
+            df.to_csv (f"{output}", index=False)
+            print(f"File {output} salvato")
+            return
+        case _:
+            raise Exception ("Errore, devi specificare un tipo di subset")
+                
 
 
 if __name__ == "__main__":
@@ -53,9 +70,10 @@ if __name__ == "__main__":
     parser.add_argument("filename", type=str, help="il nome del file da cui vuoi estrarre i dati")
     parser.add_argument("-row", type=int,  help="il numero di righe che vuoi estrarre dal file")
     parser.add_argument("-out", type=str, help="il nome del file di output")
+    parser.add_argument("-data", type=str, help="il tipo di subset che vuoi creare")
     
     args = parser.parse_args()
     
     print(f"Avvio estrazione di {args.row} da {args.filename}")
 
-    main(args.filename, args.row, args.out)
+    main(args.filename, args.row, args.out, args.data)
